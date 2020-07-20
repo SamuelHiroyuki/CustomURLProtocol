fc1d665ca0068a6522596e13368511ec47af175b
SamuelHiroyuki <samduasvezesmortal@gmail.com>
2020-07-20 16:48:51 -0300

    Verificação dos parametros desejados

--- CustomURLProtocol/Program.cs
+++ CustomURLProtocol/Program.cs
@@ -13,22 +13,57 @@ namespace CustomURLProtocol
         static void Main(string[] args)
         {
             string appName = "CustomURLProtocol";
-            string appPath = @"C:\Users\Samuel Hiroyuki\source\repos\CustomURLProtocol\CustomURLProtocol\bin\Debug\netcoreapp3.1\CustomURLProtocol.exe";
+            //string appPath = @"C:\Users\Samuel Hiroyuki\source\repos\CustomURLProtocol\CustomURLProtocol\bin\Debug\netcoreapp3.1\CustomURLProtocol.exe";
 
-            RegistryKey key = Registry.ClassesRoot.OpenSubKey(appName);  //open myApp protocol's subkey
+            //RegistryKey key = Registry.ClassesRoot.OpenSubKey(appName);  //open myApp protocol's subkey
 
-            if (key == null)  //if the protocol is not registered yet...we register it
+            //if (key == null)  //if the protocol is not registered yet...we register it
+            //{
+            //    key = Registry.ClassesRoot.CreateSubKey(appName);
+            //    key.SetValue(string.Empty, "URL: " + appName + " Protocol");
+            //    key.SetValue("URL Protocol", string.Empty);
+
+            //    key = key.CreateSubKey(@"shell\open\command");
+            //    key.SetValue(string.Empty, appPath + " " + "%1");
+            //    //%1 represents the argument - this tells windows to open this program with an argument / parameter
+            //}
+
+            //key.Close();
+
+            try
             {
-                key = Registry.ClassesRoot.CreateSubKey(appName);
-                key.SetValue(string.Empty, "URL: " + appName + " Protocol");
-                key.SetValue("URL Protocol", string.Empty);
+                Regex pattern = new Regex(appName.ToLower() + @"://([\w\W]+)");
+                string match = Array.Find(args, argument => pattern.IsMatch(argument));
 
-                key = key.CreateSubKey(@"shell\open\command");
-                key.SetValue(string.Empty, appPath + " " + "%1");
-                //%1 represents the argument - this tells windows to open this program with an argument / parameter
+                if (!string.IsNullOrWhiteSpace(match))
+                {
+                    Regex tokenPattern = new Regex(@"\?token=(.*?);");
+                    Regex pedidoIdPattern = new Regex(@"\?pedidoId=(.*?);");
+
+                    string token = (tokenPattern.Match(match).Groups[1]).ToString();
+                    string pedidoId = (pedidoIdPattern.Match(match).Groups[1]).ToString();
+                    
+                    if (string.IsNullOrWhiteSpace(token))
+                    {
+                        Console.WriteLine("É necessário informar um Token.");
+                    }
+
+                    if (string.IsNullOrWhiteSpace(pedidoId))
+                    {
+                        Console.WriteLine("É necessário informar um Código de pedido.");
+                    }
+
+                    Console.WriteLine(token);
+                    Console.WriteLine(pedidoId);
+                }
+
+                Console.ReadLine();
             }
-
-            key.Close();
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
         }
     }
 }
